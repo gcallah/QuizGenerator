03443acb2cbdec55b703153b28dcab3210b89b8a
DavidCSlakter <davidcslakter@8e86bfb0e784>
2021-03-11 21:42:29 +0000

    changed settings.py

--- QuizGenerator/settings.py
+++ QuizGenerator/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'questions.apps.QuestionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
