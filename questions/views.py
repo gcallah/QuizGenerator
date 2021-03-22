9cc3214a6298baf914523fdd0552aac35453fe73
Alex Shpyrko <alex.shpyrko@gmail.com>
2021-03-22 16:17:59 -0400

    Renders a basic index/home page

--- questions/views.py
+++ questions/views.py
@@ -4,6 +4,9 @@ from questions.models import Question
 
 # Create your views here.
 def index(request):
+    return render(request, 'index.html')
+
+def random_question(request):
     q = Question.objects.get(pk=1)
     return HttpResponse("random question: ", q.question_text)
 
