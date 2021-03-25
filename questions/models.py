01ca7eb1347d2e0cfb629090d81eae61cdbd765c
Alex Shpyrko <alex.shpyrko@gmail.com>
2021-03-25 15:43:42 -0400

    Added course model

--- questions/models.py
+++ questions/models.py
@@ -25,3 +25,11 @@ class Module(models.Model):
 
     def __str__(self):
         return self.name
+
+class Course(models.Model):
+    title = models.TextField()
+    num_of_modules = models.IntegerField()
+    semester = models.CharField(max_length=16)
+
+    def _str__(self):
+        return self.title
