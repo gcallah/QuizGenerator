358277f8a06c2b4fdfa849ea0980375db6c7004b
Mousta Taenchiki <45579563+Moustadirane@users.noreply.github.com>
2021-04-10 10:07:59 -0400

    Update models.py

--- questions/models.py
+++ questions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 
 
-class Question(models.Model):
+class Question(models.Model): #This class creates a question.
     question_text = models.CharField(max_length=200)
     
     def __str__(self):
@@ -14,7 +14,7 @@ class Question(models.Model):
     def edit(self):
         question_text = models.CharField(max_length = 200)
 
-class Choice(models.Model):
+class Choice(models.Model): #Creates choices for the questions 
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
     choice_text = models.CharField(max_length=200)
     is_answer = models.BooleanField(default=False)
@@ -27,7 +27,7 @@ class Choice(models.Model):
         choice_text = models.CharField(max_length=200)
         is_answer = models.BooleanField(default=False)
 
-class Course(models.Model):
+class Course(models.Model): #Creates a course for use by a professor 
     title = models.TextField()
     semester = models.CharField(max_length=16)
 
@@ -41,7 +41,7 @@ class Course(models.Model):
         title = models.TextField()
         semester = models.CharField(max_length = 16)
 
-class Module(models.Model):
+class Module(models.Model): #This creates a module for courses, can be tied to multiple courses and have multiple questions
     questions = models.ManyToManyField(Question)
     name = models.CharField(max_length=200)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules', default='')
@@ -54,13 +54,13 @@ class Module(models.Model):
         name = models.CharField(max_length = 200)
         course = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = 'modules', default = '')
 
-class Quiz(models.Model):
+class Quiz(models.Model): #Creates a quiz for courses.
     questions = models.ManyToManyField(Question)
     name = models.CharField(max_length = 200)
     module = models.ForeignKey(Module, on_delete = models.CASCADE, related_name = 'quizzes', default = '')
     course = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = 'quizzes', default = '')
 
-    def __str__(self):
+    def __str__(s.lf):
         return self.name
     
     def get_questions(self):
