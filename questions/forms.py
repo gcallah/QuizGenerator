3c78b7456ec6050dd325567d9cad65da56f3cf76
Nidhin Nishanth <nidhinnish@gmail.com>
2021-04-10 22:01:16 -0400

    adding ModuleForm to forms.py

--- questions/forms.py
+++ questions/forms.py
@@ -6,3 +6,6 @@ class courseForm(forms.Form):
     CHOICES = [('spring', 'spring'), ('fall', 'fall')]
     course_semester = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
     course_year = forms.IntegerField(label="year", max_value=2099, min_value=2000)
+
+class ModuleForm(forms.Form):
+    module_name = forms.CharField(label = 'Module Name', max_length = 100)
